229a96ba55e57e0c6e84dab2aadf62e366dbc3ab
obayed <obayed@eatlbd.com>
2021-10-19 18:52:19 +0600

    password encoder added

---
+++ src/main/java/com/learn/springsecuritylearn/config/MySecurityConfig.java
@@ -0,0 +1,36 @@
+package com.learn.springsecuritylearn.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder =
+                PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        auth.inMemoryAuthentication().withUser("Rahim").password(encoder.encode("123")).roles("NORMAL");
+       auth.inMemoryAuthentication().withUser("Rashed").password(encoder.encode("abc")).roles("ADMIN");
+
+    }
+
+}
